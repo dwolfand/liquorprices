93dccd7b7a3b8201f06e4f16019e5c647543dc8d
David Wolfand <dwolfand@hotmail.com>
2014-04-28 19:02:04 -0400

    got the loaddb stuff working woo

--- web.js
+++ web.js
@@ -2,69 +2,73 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var request = require('request');
-var pg = require('pg');
+var pg = require('pg.js');
+var mongo = require('mongodb');
 var app = express();
-
+var mongoUri = process.env.MONGOHQ_URL || 'mongodb://david:liquorpricespass@localhost:10062/liquorprices';
+		
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-	
-var payload = {
-	'displaycnt':10,
-	'keyword':'',
-	'pricemin':'0',
-	'pricemax':'1000',
-	'category':'null',
-	'categorymain':'',
-	'size':'null',
-	'isSale':'0',
-	'sortid':'longdescription'
-	},
-	options = {
-    url: 'http://www2.montgomerycountymd.gov/dlcsearch/SearchSupportService.asmx/GetSearchData',
-	method: 'POST',
-	//proxy : 'http://127.0.0.1:8888',
-	body: payload,
-	json: true,
-    headers: {
-        'User-Agent': 'request'
+	// mongo.Db.connect(mongoUri, function (err, db) {
+		// db.collection('liquorprices', function(er, collection) {
+			
+		// });
+	// });
+	res.send('nothing right now');
+});
+
+app.get('/loaddb', function(req, res) {
+
+	var payload = {
+		'displaycnt':0,
+		'keyword':'',
+		'pricemin':'0',
+		'pricemax':'10000',
+		'category':'null',
+		'categorymain':'',
+		'size':'null',
+		'isSale':'0',
+		'sortid':'longdescription'
+		};
+	var options = {
+		url: 'http://www2.montgomerycountymd.gov/dlcsearch/SearchSupportService.asmx/GetSearchData',
+		method: 'POST',
+		//proxy : 'http://127.0.0.1:8888',
+		body: payload,
+		json: true
+		};
+
+	function callback(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log('success');
+			//console.log(body.d);
+			var size = 0, key;
+			
+			mongo.Db.connect(mongoUri, function (err, db) {
+				db.collection('liquorprices', function(er, collection) {
+					collection.remove({},function(err, removed){
+						for (key in body.d) {
+							size++;
+							collection.insert(body.d[key], {w:1}, function(er,rs) {
+								//console.log('saved this one');
+								//console.log(body.d[key]);
+							});
+						}
+						res.send('done saving '+size+' items into the db');
+					});
+				});
+			});
+			
+		}
+		else{
+			console.log('ERROR!');
+			console.log(error);
 		}
 	};
 
-function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-		console.log('success');
-        console.log(body.d);
-		var size = 0, key;
-		for (key in body.d) {
-			size++;
-		}
-		console.log(size);
-		res.send(body.d);
-    }
-	else{
-		console.log('ERROR!');
-		console.log(error);
-	}
-};
-
-request(options, callback);
-
-
-  // res.send('hello');
-});
-
-app.get('/dbtest', function(req, res) {
-console.log("here");
-pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-  client.query('SELECT * FROM films', function(err, result) {
-    done();
-    if(err) res.send(err);
-    res.send(result.rows);
-  });
-});
-
-  // res.send('hello');
+	request(options, callback);
+   
 });
 
 var port = Number(process.env.PORT || 5000);
