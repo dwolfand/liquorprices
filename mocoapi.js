6e094e89a639a9b0d102ca4b94f1262657c7a810
David Wolfand <dwolfand@hotmail.com>
2014-04-29 16:17:55 -0400

    changing query count to unlimited for actual load

--- mocoapi.js
+++ mocoapi.js
@@ -3,7 +3,7 @@ var request = require('request');
 module.exports = {
   getAllLiquors: function (curDate, callback) {
     	var payload = {
-		'displaycnt':10,
+		'displaycnt':0,
 		'keyword':'',
 		'pricemin':'0',
 		'pricemax':'10000',
